10ff0951ad6f78fb2e7f3128b9f47d9e8d54a00c
Drunken Cat <songjh123123@outlook.com>
2023-05-14 07:40:46 +0800

    给界面上加了滑条。准备重构这一页代码。

--- lib/widgets/simple_scroll_control.dart
+++ lib/widgets/simple_scroll_control.dart
@@ -4,7 +4,38 @@ import 'package:provider/provider.dart';
 import '../models/slate_num_notifier.dart';
 
 // the button to increment the counter for every column， add and scroll different columns 
-class IncrementCounterButton<T extends SlatePickerState> extends StatelessWidget {
+class SliderValueController<T extends SlatePickerState> {
+
+  final TextEditingController textCon;
+  BuildContext context;
+  VoidCallback? inc;
+  VoidCallback? dec;
+
+
+  SliderValueController({
+    required this.context,
+    required this.textCon,
+    this.inc,
+    this.dec,
+  });
+
+  void valueInc(){
+    inc?.call();
+    textCon.clear();
+    var _ = Provider.of<T>(context,listen: false);
+    _.scrollToNext();
+  }
+
+  void valueDec(){
+    dec?.call();
+    textCon.clear();
+    var _ = Provider.of<T>(context,listen: false);
+    _.scrollToPrev();
+  }
+
+}
+
+class IncrementCounterButton<T extends SlatePickerState> extends StatelessWidget{
   final VoidCallback onPressed;
   final TextEditingController textCon;
 
