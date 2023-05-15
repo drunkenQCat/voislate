88624b487ecce7ad8fe39b1d39895b872c8114f6
Drunken Cat <songjh123123@outlook.com>
2023-05-15 21:59:26 +0800

    上次提交的补充

--- lib/models/slate_schedule.dart
+++ lib/models/slate_schedule.dart
@@ -5,9 +5,10 @@ import 'package:flutter/material.dart';
 class ScheduleItem{
   final String key;
   final String fix;
+  final String name;
   final Note note;
   // constructor
-  ScheduleItem(this.key, this.fix, this.note);
+  ScheduleItem(this.key, this.fix, this.note):name = key + fix;
 }
 
 class Note {
@@ -16,22 +17,36 @@ class Note {
   final String append;
   //constructor
   Note({ required this.objects, required this.type, required this.append });
+  @override
+  String toString() {
+    // concatenate the objects list into a string
+    String objectsString = objects.join(', ');
+    return 'Note{objects: $objectsString, type: $type, append: $append}';
+  }
 }
 // this abstract class is used to store the data of the schedule, literally a list of schedule items
-abstract class DataList with ChangeNotifier{
+class DataList extends ChangeNotifier{
   late List<ScheduleItem> _data;
   List<ScheduleItem> get data => _data;
   set data(List<ScheduleItem> data){
+    // detect that if the _data has duplicate items
+    // if it does, throw an error
+    var set = <String>{};
+    for (var item in data) {
+      if (set.contains(item.name)) {
+        throw Exception('Duplicate items in the list');
+      }
+      set.add(item.name);
+    }
     _data = data;
     refresh();
   }
 
-  ScheduleItem operator [](int index) => _data[index];
-
   get length {
     return _data.length;}
 
-  void add(ScheduleItem item){
+
+  void add(ScheduleItem item, [ ShotSchedule? shots ]){
     _data.add(item);
     refresh();
   }
@@ -41,8 +56,14 @@ abstract class DataList with ChangeNotifier{
     refresh();
   }
 
-  void removeAt(int index){
-    _data.removeAt(index);
+  ScheduleItem removeAt(int index){
+    var removed = _data.removeAt(index);
+    refresh();
+    return removed;
+  }
+
+  void insert(int index, ScheduleItem item, [ ShotSchedule? shots ]){
+    _data.insert(index, item);
     refresh();
   }
 
@@ -57,61 +78,63 @@ abstract class DataList with ChangeNotifier{
   }
 }
 
+class ShotSchedule extends DataList{
+  ScheduleItem operator [](int index) => data[index];
+  ShotSchedule(List<ScheduleItem> inputList){
+    data = inputList;
+  }
+
+}
+
 class SceneSchedule extends DataList{
-  List<ShotSchedule> _ = [];
   Map<String, ShotSchedule> shotScheduleMap = {};
 
+  ShotSchedule operator [](int index){
+    return shotScheduleMap[_data[index].name]!;
+  }
+
   SceneSchedule({ required List<ScheduleItem> list, required List<ShotSchedule> shots }){
-    _data = list;
+    data = list;
     assert(shots.length == list.length);
-    _ = shots;
+    var _ = shots;
     // map data to _
-    for (var item in _data) {
+    for (var item in data) {
       // just pop the data out 
       var currentScene = _.removeAt(0);
-      shotScheduleMap[item.key] = currentScene;
+      shotScheduleMap[item.name] = currentScene;
     }
-    @override
-    void add(ScheduleItem item) {
-      shotScheduleMap[item.key] = ShotSchedule([]);
-      super.add(item);
-    }
+  }
+  @override
+  void add(ScheduleItem item, [ShotSchedule? shots]) {
+    shotScheduleMap[item.name] = shots!;
+    super.add(item);
+  }
 
-    @override
-    void remove(ScheduleItem item) {
-      shotScheduleMap.remove(item.key);
-      super.remove(item);
-    }
+  @override
+  void remove(ScheduleItem item) {
+    shotScheduleMap.remove(item.name);
+    super.remove(item);
+  }
 
-    @override
-    void update(ScheduleItem oldItem, ScheduleItem newItem) {
-      shotScheduleMap[newItem.key] = shotScheduleMap[oldItem.key]!;
-      shotScheduleMap.remove(oldItem.key);
-      super.update(oldItem, newItem);
-    }
+  @override
+  ScheduleItem removeAt(int index){
+    var removed = super.removeAt(index);
+    shotScheduleMap.remove(removed);
+    return removed;
+
+  }
+
+  @override
+  void insert(int index, ScheduleItem item, [ ShotSchedule? shots ]){
+    shotScheduleMap[item.name] = shots!;
+    super.insert(index, item);
+  }
+
+  @override
+  void update(ScheduleItem oldItem, ScheduleItem newItem,[ ShotSchedule? shots ]) {
+    shotScheduleMap[newItem.name] = shots!;
+    shotScheduleMap.remove(oldItem.name);
+    super.update(oldItem, newItem);
   }
 }
 
-class ShotSchedule extends DataList{
-  ShotSchedule(List<ScheduleItem> inputList){
-    _data = inputList;
-  }
-}
-
-// make an instance of SceneSchedule
-// create a list of ScheduleItem objects
-List<ScheduleItem> scheduleItems = [
-  ScheduleItem('1', 'fix1', Note(objects: ['object1', 'Hero', 'Crowd'], type: 'type1', append: 'append1')),
-  ScheduleItem('2', 'fix2', Note(objects: ['object2'], type: 'type2', append: 'append2')),
-  ScheduleItem('3', 'fix3', Note(objects: ['object3'], type: 'type3', append: 'append3')),
-];
-
-// create a list of ShotSchedule objects
-List<ShotSchedule> shotSchedules = [
-  ShotSchedule([ScheduleItem('4', 'fix4', Note(objects: ['object4'], type: 'type4', append: 'append4'))]),
-  ShotSchedule([ScheduleItem('5', 'fix5', Note(objects: ['object5'], type: 'type5', append: 'append5'))]),
-  ShotSchedule([ScheduleItem('6', 'fix6', Note(objects: ['object6'], type: 'type6', append: 'append6'))]),
-];
-
-// create a SceneSchedule object
-SceneSchedule sceneSchedule = SceneSchedule(list: scheduleItems, shots: shotSchedules);
--- lib/pages/scene_schedule_page.dart
+++ lib/pages/scene_schedule_page.dart
@@ -6,7 +6,6 @@ class SceneSchedulePage extends StatefulWidget {
   _SceneSchedulePageState createState() => _SceneSchedulePageState();
 }
 
-
 /*
 接下来的这一页的规划：
 1. 一个左边的列表，右边的详情
@@ -19,96 +18,235 @@ class SceneSchedulePage extends StatefulWidget {
 */
 class _SceneSchedulePageState extends State<SceneSchedulePage> {
   int _selectedIndex = 0;
+  int _selectedShotIndex = 0;
 
-  
-  final SceneSchedule sceneSchedule = SceneSchedule(
+  final SceneSchedule scenes = SceneSchedule(
     list: [
-      ScheduleItem('1', 'Fix 1', Note(objects: ['Object 1'], type: 'Type 1', append: 'Append 1')),
-      ScheduleItem('2', 'Fix 2', Note(objects: ['Object 2'], type: 'Type 2', append: 'Append 2')),
-      ScheduleItem('3', 'Fix 3', Note(objects: ['Object 3'], type: 'Type 3', append: 'Append 3')),
+      ScheduleItem('1', 'A',
+          Note(objects: ['Object 1'], type: 'Type 1', append: 'Append 1')),
+      ScheduleItem('2', 'A',
+          Note(objects: ['Object 2'], type: 'Type 2', append: 'Append 2')),
+      ScheduleItem('3', 'A',
+          Note(objects: ['Object 3'], type: 'Type 3', append: 'Append 3')),
     ],
     shots: [
       ShotSchedule([
-        ScheduleItem('1', 'Fix 1', Note(objects: ['Object 1'], type: 'Type 1', append: 'Append 1')),
-        ScheduleItem('2', 'Fix 2', Note(objects: ['Object 2'], type: 'Type 2', append: 'Append 2')),
-        ScheduleItem('3', 'Fix 3', Note(objects: ['Object 3'], type: 'Type 3', append: 'Append 3')),
-        ScheduleItem('4', 'Fix 4', Note(objects: ['Object 4'], type: 'Type 4', append: 'Append 4')),
-        ScheduleItem('5', 'Fix 5', Note(objects: ['Object 5'], type: 'Type 5', append: 'Append 5')),
-        
-        ]),
+        ScheduleItem('1', 'A',
+            Note(objects: ['Object 1'], type: 'Type 1', append: 'Append 1')),
+        ScheduleItem('2', 'A',
+            Note(objects: ['Object 2'], type: 'Type 2', append: 'Append 2')),
+        ScheduleItem('3', 'A',
+            Note(objects: ['Object 3'], type: 'Type 3', append: 'Append 3')),
+        ScheduleItem('4', 'A',
+            Note(objects: ['Object 4'], type: 'Type 4', append: 'Append 4')),
+        ScheduleItem('5', 'A',
+            Note(objects: ['Object 5'], type: 'Type 5', append: 'Append 5')),
+      ]),
       ShotSchedule([
-        ScheduleItem('2', 'Fix 2', Note(objects: ['Object 2'], type: 'Type 2', append: 'Append 2')),
-        ScheduleItem('6', 'Fix 6', Note(objects: ['Object 6'], type: 'Type 6', append: 'Append 6')),
-        ScheduleItem('7', 'Fix 7', Note(objects: ['Object 7'], type: 'Type 7', append: 'Append 7')),
-        ScheduleItem('8', 'Fix 8', Note(objects: ['Object 8'], type: 'Type 8', append: 'Append 8')),
-        ScheduleItem('9', 'Fix 9', Note(objects: ['Object 9'], type: 'Type 9', append: 'Append 9')),
-        ]),
+        ScheduleItem('2', 'A',
+            Note(objects: ['Object 2'], type: 'Type 2', append: 'Append 2')),
+        ScheduleItem('6', 'A',
+            Note(objects: ['Object 6'], type: 'Type 6', append: 'Append 6')),
+        ScheduleItem('7', 'A',
+            Note(objects: ['Object 7'], type: 'Type 7', append: 'Append 7')),
+        ScheduleItem('8', 'A',
+            Note(objects: ['Object 8'], type: 'Type 8', append: 'Append 8')),
+        ScheduleItem('9', 'A',
+            Note(objects: ['Object 9'], type: 'Type 9', append: 'Append 9')),
+      ]),
       ShotSchedule([
-        ScheduleItem('3', 'Fix 3', Note(objects: ['Object 3'], type: 'Type 3', append: 'Append 3')),
-        ScheduleItem('10', 'Fix 10', Note(objects: ['Object 10'], type: 'Type 10', append: 'Append 10')),
-        ScheduleItem('11', 'Fix 11', Note(objects: ['Object 11'], type: 'Type 11', append: 'Append 11')),
-        ScheduleItem('12', 'Fix 12', Note(objects: ['Object 12'], type: 'Type 12', append: 'Append 12')),
-        ScheduleItem('13', 'Fix 13', Note(objects: ['Object 13'], type: 'Type 13', append: 'Append 13')),
-        ScheduleItem('14', 'Fix 14', Note(objects: ['Object 14'], type: 'Type 14', append: 'Append 14')),
-        ]),
+        ScheduleItem('3', 'A',
+            Note(objects: ['Object 3'], type: 'Type 3', append: 'Append 3')),
+        ScheduleItem('10', 'A',
+            Note(objects: ['Object 10'], type: 'Type 10', append: 'Append 10')),
+        ScheduleItem('11', 'A',
+            Note(objects: ['Object 11'], type: 'Type 11', append: 'Append 11')),
+        ScheduleItem('12', 'A',
+            Note(objects: ['Object 12'], type: 'Type 12', append: 'Append 12')),
+        ScheduleItem('13', 'A',
+            Note(objects: ['Object 13'], type: 'Type 13', append: 'Append 13')),
+        ScheduleItem('14', 'A',
+            Note(objects: ['Object 14'], type: 'Type 14', append: 'Append 14')),
+      ]),
     ],
   );
 
-  final _items = List<String>.generate(20, (i) => 'Item ${i + 1}');
   @override
   Widget build(BuildContext context) {
+    var leftWidth = MediaQuery.of(context).size.width * 0.3;
     return Row(
       children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: sceneSchedule.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Dismissible(
-                key:Key(sceneSchedule[index].key + sceneSchedule[index].fix),
-                onDismissed: (direction) => setState(() {
-                  
-                  if(direction == DismissDirection.endToStart){
-                    sceneSchedule.removeAt(index);
-                  }else if(direction == DismissDirection.startToEnd){
-                    sceneSchedule.removeAt(index);
-                    _showModalBottomSheet(context);
-                  }
-                }),
-                secondaryBackground: Container(
-                  color: Colors.red,
-                  child: const Icon(Icons.delete),
-                ),
-                background: Container(
-                  color: Colors.green,
-                  child: const Icon(Icons.edit),
-                ),
-                child: ListTile(
-                  title: Text(_items[index]),
-                  selected: index == _selectedIndex,
-                  onTap: () {
-                    setState(() {
-                      _selectedIndex = index;
-                    });
-                  },
-                ),
+        Flexible(
+          flex: 1,
+          child: ListView.separated(
+            itemCount: scenes.length,
+            separatorBuilder: (context, index) {
+              return const Divider(
+                height: 1,
+                color: Colors.grey,
               );
+            },
+            itemBuilder: (BuildContext context, int index) {
+              return leftList(index, context);
             },
           ),
         ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            color: Colors.grey[200],
-            child: Center(
-              child: Text(
-                _items[_selectedIndex],
-                style: const TextStyle(fontSize: 24),
-              ),
-            ),
+        Flexible(
+          // a copy of listview above , but to display the shot schedule
+          flex: 3,
+          child: ListView.builder(
+            itemCount: scenes[_selectedIndex].length,
+            itemBuilder: (BuildContext context, int index2) {
+              return rightList(index2, context);
+            },
           ),
         ),
       ],
     );
+  }
+
+  Dismissible rightList(int index, BuildContext context) {
+    var itemGroup = scenes[_selectedIndex];
+    var item = itemGroup[index];
+
+    return Dismissible(
+      key: Key(item.name),
+      onDismissed: (direction) => setState(() {
+        if (direction == DismissDirection.endToStart) {
+          removeItem(index, context);
+        }
+      }),
+      confirmDismiss: (direction) async {
+        if (direction == DismissDirection.endToStart) {
+          return true;
+        } else if (direction == DismissDirection.startToEnd) {
+          _showModalBottomSheet(context);
+          return false;
+        }
+        return null;
+      },
+      secondaryBackground: Container(
+        color: Colors.red,
+        child: const Icon(Icons.delete),
+      ),
+      background: Container(
+        color: Colors.green,
+        child: const Icon(Icons.edit),
+      ),
+      child: LongPressDraggable(
+        data: item,
+        feedback: ListTile(
+          title: Text(item.name),
+          subtitle: Text(item.note.type),
+          focusColor: Colors.amber,
+          selected: index == _selectedShotIndex,
+          onTap: () {
+            setState(() {
+              _selectedShotIndex = index;
+            });
+          },
+        ),
+        childWhenDragging: Container(),
+        child: ListTile(
+          title: Text(item.name),
+          subtitle: Text(item.note.toString()),
+          selected: index == _selectedShotIndex,
+          onTap: () {
+            setState(() {
+              _selectedShotIndex = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  Dismissible leftList(int index, BuildContext context) {
+    var item = scenes[index];
+    return Dismissible(
+      key: Key(scenes.data[index].name),
+      onDismissed: (direction) => setState(() {
+        if (direction == DismissDirection.endToStart) {
+          removeItem(index, context, item);
+        }
+      }),
+      confirmDismiss: (direction) async {
+        if (direction == DismissDirection.endToStart) {
+          return true;
+        } else if (direction == DismissDirection.startToEnd) {
+          _showModalBottomSheet(context);
+          return false;
+        }
+        return null;
+      },
+      secondaryBackground: Container(
+        color: Colors.red,
+        child: const Icon(Icons.delete),
+      ),
+      background: Container(
+        color: Colors.green,
+        child: const Icon(Icons.edit),
+      ),
+      child: LongPressDraggable(
+        data: scenes,
+        feedback: ListTile(
+          leading: CircleAvatar(
+            child: Text(scenes.data[index].name
+          ),),
+          focusColor: Colors.amber,
+          selected: index == _selectedIndex,
+          onTap: () {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
+        childWhenDragging: Container(),
+        child: ListTile(
+          leading: Column(
+            children: [
+              CircleAvatar(
+                child: Text(scenes.data[index].name),
+              ),
+              Text(scenes.data[index].note.type),
+            ],
+          ),
+          selected: index == _selectedIndex,
+          onTap: () {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  void removeItem(int index, BuildContext context, [ShotSchedule? item]) {
+    // if modify shot schedule, item is null
+    if (item != null) {
+      var removed = scenes.removeAt(index);
+      _selectedIndex = (index - 1 < 0) ? 0 : index - 1;
+      _selectedShotIndex = 0;
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('${removed.name} dismissed'),
+          action: SnackBarAction(
+              label: 'UNDO',
+              onPressed: () {
+                scenes.insert(index, removed, item);
+              })));
+    } else {
+      var removed = scenes[_selectedIndex].removeAt(index);
+      // if remove the last item, _selectedShotIndex will be -1
+      _selectedShotIndex = (index - 1 < 0) ? 0 : index - 1;
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('${removed.name} dismissed'),
+          action: SnackBarAction(
+              label: 'UNDO',
+              onPressed: () {
+                scenes[_selectedShotIndex].insert(index, removed);
+              })));
+    }
   }
 }
 
