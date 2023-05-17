532d2134ecb169c4c22542a5c5b16990673d3662
Drunken Cat <songjh123123@outlook.com>
2023-05-18 03:34:01 +0800

    大致重构了recordpage，用AI辅助写了场记页的框架

--- lib/pages/slate_log_page.dart
+++ lib/pages/slate_log_page.dart
@@ -1,139 +1,150 @@
 import 'package:flutter/material.dart';
 
 import '../models/slate_schedule.dart';
-import '../data/dummy_scenes.dart';
+import '../models/slate_log_item.dart';
 // give me a frame of listview page
 class SlateLog extends StatefulWidget {
+  const SlateLog({super.key});
+
   @override
   _SlateLogState createState() => _SlateLogState();
 }
 
-class _SlateLogState extends State<SlateLog> with AutomaticKeepAliveClientMixin{
-  late TextEditingController _fixController;
-  late TextEditingController _noteController;
+class _SlateLogState extends State<SlateLog> {
+  List<SlateLogItem> slateLogItems = [
+    SlateLogItem(
+      scn: 'Scene 1',
+      sht: 'Shot 1',
+      tk: 1,
+      filenamePrefix: 'Prefix 1',
+      filenameLinker: 'Linker 1',
+      filenameNum: 1,
+      tkNote: 'TK Note 1',
+      shtNote: 'Shot Note 1',
+      scnNote: 'Scene Note 1',
+      okTk: TkStatus.ok,
+      okSht: ShtStatus.ok,
+    ),
+    SlateLogItem(
+      scn: 'Scene 2',
+      sht: 'Shot 2',
+      tk: 2,
+      filenamePrefix: 'Prefix 2',
+      filenameLinker: 'Linker 2',
+      filenameNum: 2,
+      tkNote: 'TK Note 2',
+      shtNote: 'Shot Note 2',
+      scnNote: 'Scene Note 2',
+      okTk: TkStatus.bad,
+      okSht: ShtStatus.nice,
+    ),
+    SlateLogItem(
+      scn: 'Scene 2',
+      sht: 'Shot 1',
+      tk: 3,
+      filenamePrefix: 'Prefix 3',
+      filenameLinker: 'Linker 3',
+      filenameNum: 3,
+      tkNote: 'TK Note 3',
+      shtNote: 'Shot Note 3',
+      scnNote: 'Scene Note 3',
+      okTk: TkStatus.notChecked,
+      okSht: ShtStatus.ok,
+    ),
+  ];
 
-  @override
-  bool get wantKeepAlive => true;
-
-  @override
-  void initState() {
-    super.initState();
-    _fixController = TextEditingController(text: defaultScenes.data[0].fix);
-    _noteController = TextEditingController(text: defaultScenes.data[0].note.toString());
-  }
-
-  @override
-  void dispose() {
-    _fixController.dispose();
-    _noteController.dispose();
-    super.dispose();
-  }
-  List<Widget> objectsEditor(int index){
-    List<Widget> chipList = defaultScenes.data[index].note.objects.asMap().map(
-      (idx, tag) => MapEntry(
-        idx,
-        TextButton(
-          onPressed: (){
-            setState(() {
-                tagButton(index, tag, idx);
-              });},
-          child: Chip(label: Text(tag)
-          )),
-      )
-      ).values.toList();
-
-    chipList.add(TextButton(
-          
-          onPressed: (){
-            setState(() {
-              defaultScenes.data[index].note.objects.add('new');
-            });
-          },
-          child: const Chip(
-            label: Icon(
-                Icons.add,
-                ),
-            )
-          ),
-    );
-    return chipList;
-  }
-
-  void tagButton(int index, String tag, int idx) {
-    showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: const Text('Confirm delete'),
-        content: TextFormField(
-          initialValue: defaultScenes.data[index].note.objects[idx],
-          onSaved: ((value) {
-            if (value != null) {
-              defaultScenes.data[index].note.objects[idx] = value;
-            }
-          }),
-          decoration: const InputDecoration(labelText: 'Tag'),
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(false);
-            },
-            child: const Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () {
-              // FormState.save();
-              Navigator.of(context).pop(false);
-            },
-            child: const Text('Save'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-              setState(
-                  () => defaultScenes.data[index].note.objects.remove(tag));
-            },
-            child: const Text('Delete'),
-          ),
-        ],
-      ),
-    );
-    // .then(
-    //   (confirmed) => {
-    //     if (confirmed)
-    //       {
-    //         defaultScenes.data[index].note.objects.remove(tag),
-    //         // Provider.of<Users>(context, listen: false).remove(user),
-    //       },
-    //   },
-    // );
-  }
+  Set<String> foldedScenes = {};
 
   @override
   Widget build(BuildContext context) {
-    super.build(context);
-    return Column(
-      children: [
-        // TextFormField(
-        //   decoration: InputDecoration(labelText: 'Key'),
-        //   initialValue: defaultScenes.data[0].key,
-        //   onChanged: (value) => widget.onScheduleItemChanged(defaultScenes.data[0].copyWith(key: value)),
-        // ),
-        // TextFormField(
-        //   decoration: InputDecoration(labelText: 'Fix'),
-        //   controller: _fixController,
-        //   onChanged: (value) => widget.onScheduleItemChanged(defaultScenes.data[0].copyWith(fix: value)),
-        // ),
-        // TextFormField(
-        //   decoration: InputDecoration(labelText: 'Note'),
-        //   controller: _noteController,
-        //   onChanged: (value) => widget.onScheduleItemChanged(defaultScenes.data[0].copyWith(note: Note.fromString(value))),
-        // ),
-        Wrap(
-          spacing: 8.0,
-          children: objectsEditor(0),
-        ),
-      ],
+    return ListView.builder(
+      itemCount: slateLogItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        SlateLogItem item = slateLogItems[index];
+        bool isFolded = foldedScenes.contains(item.scn);
+        bool isSelected = false; // Add the logic for selecting a list item
+
+        return Column(
+          children: [
+            ListTile(
+              onTap: () {
+                // Toggle the folding state of the scene
+                setState(() {
+                  if (isFolded) {
+                    foldedScenes.remove(item.scn);
+                  } else {
+                    foldedScenes.add(item.scn);
+                  }
+                });
+              },
+              tileColor: isSelected ? Colors.blue.shade200 : null,
+              leading: CircleAvatar(
+                child: Text(item.tk.toString()),
+              ),
+              title: RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        ),
+                  children: [
+                    TextSpan(
+                      text: item.filenamePrefix,
+                    ),
+                    TextSpan(text: ' '),
+                    TextSpan(text: item.filenameLinker),
+                    TextSpan(text: ' '),
+                    TextSpan(text: item.filenameNum.toString()),
+                  ],
+                ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('TK Note: ${item.tkNote}'),
+                  Text('Shot Note: ${item.shtNote}'),
+                  Text('Scene Note: ${item.scnNote}'),
+                ],
+              ),
+              trailing: Icon(
+                _getShtStatusIcon(item.okSht),
+                color: _getShtStatusColor(item.okSht),
+              ),
+            ),
+            if (!isFolded)
+              Divider(
+                thickness: 1.0,
+                height: 0.0,
+              ),
+          ],
+        );
+      },
     );
+  }
+
+  IconData _getShtStatusIcon(ShtStatus status) {
+    switch (status) {
+      case ShtStatus.notChecked:
+        return Icons.check_box_outline_blank;
+      case ShtStatus.ok:
+        return Icons.check_circle_outline;
+      case ShtStatus.nice:
+        return Icons.thumb_up_alt_outlined;
+      default:
+        return Icons.error_outline;
+    }
+  }
+
+  Color _getShtStatusColor(ShtStatus status) {
+    switch (status) {
+      case ShtStatus.notChecked:
+        return Colors.grey;
+      case ShtStatus.ok:
+        return Colors.green;
+      case ShtStatus.nice:
+        return Colors.blue;
+      default:
+        return Colors.red;
+    }
   }
 }
