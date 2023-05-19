bbd874d9eaceb0b8cc2514546b2e34a7b55a64b6
Drunken Cat <songjh123123@outlook.com>
2023-05-19 10:14:34 +0800

    做joystick重构以及整体程序hive重构的准备

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'pages/main_page.dart';
-void main() {
+void main() async{
+  await Hive.initFlutter();
   runApp(VoiSlate());
 }
